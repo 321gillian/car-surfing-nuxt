691e0a82e55f2a396b3e8c04124fe37faec4dd15
321gillian <gillian.middleton@gmail.com>
2023-02-28 17:36:19 +0000

    Basic create carpool page markup

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -58,14 +58,14 @@
             >
           </li>
           <li>
-            <a
+            <NuxtLink
               link="/"
               isactive="false"
               component="a"
               linkattr="href"
               class="block py-2 pr-4 pl-3 rounded md:p-0 text-gray-700 hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-teal-500 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              href="/"
-              >Create Ride</a
+              to="/rides/new"
+              >Find Ride</NuxtLink
             >
           </li>
           <li>
